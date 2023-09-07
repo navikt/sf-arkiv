423632aadf6078cb0a69500c633e25f156b4ba44
Arne Hanssen <arne.hanssen@nav.no>
2023-09-07 09:20:27 +0200

    Monitor app state, use result in internal is_ready/is_alive api

--- src/main/no/nav/sf/arkiv/Routing.kt
+++ src/main/no/nav/sf/arkiv/Routing.kt
@@ -1,28 +1,35 @@
 package no.nav.sf.arkiv
 
 import io.ktor.application.call
-import io.ktor.http.HttpStatusCode
+import io.ktor.http.HttpStatusCode.Companion.InternalServerError
+import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.prometheus.client.exporter.common.TextFormat
 import java.io.StringWriter
 
-fun Routing.podAPI() {
-
+fun Routing.podAPI(appState: ApplicationState) {
     get("/internal/is_alive") {
-        call.respond(HttpStatusCode.OK)
+        if (appState.alive) {
+            call.respond(OK)
+        } else {
+            call.respond(InternalServerError)
+        }
     }
-
     get("/internal/is_ready") {
-        call.respond(HttpStatusCode.OK)
+        if (appState.ready) {
+            call.respond(OK)
+        } else {
+            call.respond(InternalServerError)
+        }
     }
 }
 
 fun Routing.prometheusAPI() {
     get("/internal/prometheus") {
         call.respond(
-            HttpStatusCode.OK,
+            OK,
             StringWriter().let { str ->
                 TextFormat.write004(str, Metrics.cRegistry.metricFamilySamples())
                 str
